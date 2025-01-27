a251ef30d0505a2a3c14c415c3f708cbe2a0e02e
jcondotta <jefferson.condotta@gmail.com>
2025-01-27 15:52:40 +0100

    Adjustment on cards-process-service

--- cards-process-service/src/main/java/com/jcondotta/cards/process/AddCardsApplication.java
+++ cards-process-service/src/main/java/com/jcondotta/cards/process/AddCardsApplication.java
@@ -1,15 +1,12 @@
 package com.jcondotta.cards.process;
 
-import io.micronaut.context.env.Environment;
 import io.micronaut.runtime.Micronaut;
 
-//@SerdeImport(AddCardRequest.class) //TODO dar uma olhada nisso
 public class AddCardsApplication {
 
     public static void main(String[] args) {
         Micronaut.build(args)
                 .mainClass(AddCardsApplication.class)
-                .defaultEnvironments(Environment.DEVELOPMENT)
                 .start();
     }
 }
