547e028edcb0c02bd485605819926d494488c0ab
jcondotta <jefferson.condotta@gmail.com>
2025-02-02 16:30:31 +0100

    Health check IT + improvement on terraform files

--- management-service/src/main/java/com/jcondotta/cards/management/web/controller/CardApiPaths.java
+++ management-service/src/main/java/com/jcondotta/cards/management/web/controller/CardApiPaths.java
@@ -2,6 +2,6 @@ package com.jcondotta.cards.management.web.controller;
 
 public interface CardApiPaths {
 
-    String BASE_V1_PATH = "/api/v1/cards";
+    String BASE_V1_PATH = "manage/api/v1/cards";
     String BY_ID = BASE_V1_PATH + "/{card-id}";
 }
