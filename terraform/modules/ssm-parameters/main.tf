33bc406f2b21ce5528b859e47995219930aaeacf
jcondotta <jefferson.condotta@gmail.com>
2025-02-05 15:49:16 +0100

    cards ssm-parameters terraform files

--- terraform/modules/ssm-parameters/main.tf
+++ terraform/modules/ssm-parameters/main.tf
@@ -1,43 +1,27 @@
 resource "aws_ssm_parameter" "dynamodb_cards_table_ssm_param" {
-  name        = var.dynamodb_cards_table_ssm_param_name
-  description = "The name of the DynamoDB table for the cards module"
-  type        = "SecureString"
-  value       = var.dynamodb_cards_table_ssm_param_value
-
-  lifecycle {
-    create_before_destroy = true
-  }
+  name        = var.dynamodb_cards_table_param_name
+  description = "The name of the DynamoDB cards table"
+  type        = "String"
+  value       = var.dynamodb_cards_table_param_value
 }
 
 resource "aws_ssm_parameter" "dynamodb_cards_by_bank_account_id_gsi_ssm_param" {
-  name        = var.dynamodb_cards_by_bank_account_id_gsi_ssm_param_name
+  name        = var.dynamodb_cards_by_bank_account_id_gsi_param_name
   description = "The name of the GSI index for the DynamoDB cards table"
   type        = "String"
-  value       = var.dynamodb_cards_by_bank_account_id_gsi_ssm_param_value
-
-  lifecycle {
-    create_before_destroy = true
-  }
+  value       = var.dynamodb_cards_by_bank_account_id_gsi_param_value
 }
 
-resource "aws_ssm_parameter" "sqs_card_application_queue_queue_ssm_param" {
-  name        = var.sqs_card_application_queue_queue_ssm_param_name
-  description = "The name of the SQS queue for the cards module"
+resource "aws_ssm_parameter" "sqs_card_application_queue_ssm_param" {
+  name        = var.sqs_card_application_queue_param_name
+  description = "The name of the SQS card application queue"
   type        = "String"
-  value       = var.sqs_card_application_queue_queue_ssm_param_value
-
-  lifecycle {
-    create_before_destroy = true
-  }
+  value       = var.sqs_card_application_queue_param_value
 }
 
 resource "aws_ssm_parameter" "sqs_card_application_dlq_ssm_param" {
-  name        = var.sqs_card_application_dlq_ssm_param_name
-  description = "The name of the Dead Letter Queue (DLQ) for the SQS cards queue"
+  name        = var.sqs_card_application_dlq_param_name
+  description = "The name of the Dead Letter Queue (DLQ) for the SQS card application queue"
   type        = "String"
-  value       = var.sqs_card_application_dlq_ssm_param_value
-
-  lifecycle {
-    create_before_destroy = true
-  }
+  value       = var.sqs_card_application_dlq_param_value
 }
--- terraform/modules/ssm-parameters/outputs.tf
+++ terraform/modules/ssm-parameters/outputs.tf
@@ -1,22 +1,19 @@
-# Output for DynamoDB Table Name SSM Parameter
-output "dynamodb_cards_table_ssm_param_value" {
+output "dynamodb_cards_table_param_value" {
   value       = aws_ssm_parameter.dynamodb_cards_table_ssm_param.value
-  description = "The name of the SSM parameter for the DynamoDB cards table"
-}
-
-output "dynamodb_cards_by_bank_account_id_gsi_ssm_param_value" {
-  value       = aws_ssm_parameter.dynamodb_cards_by_bank_account_id_gsi_ssm_param.value
   description = "The value of the DynamoDB cards table name stored in SSM"
-  sensitive   = true # Mark as sensitive if using SecureString
 }
 
-# Output for SQS Queue Name SSM Parameter
-output "sqs_card_application_queue_queue_ssm_param_value" {
-  value       = aws_ssm_parameter.sqs_card_application_queue_queue_ssm_param.value
-  description = "The name of the SSM parameter for the SQS cards queue"
+output "dynamodb_cards_by_bank_account_id_gsi_param_value" {
+  value       = aws_ssm_parameter.dynamodb_cards_by_bank_account_id_gsi_ssm_param.value
+  description = "The value of the DynamoDB GSI for cards by bank account ID stored in SSM"
 }
 
-output "sqs_card_application_dlq_ssm_param_value" {
+output "sqs_card_application_queue_param_value" {
+  value       = aws_ssm_parameter.sqs_card_application_queue_ssm_param.value
+  description = "The value of the SQS card application queue name stored in SSM"
+}
+
+output "sqs_card_application_dlq_param_value" {
   value       = aws_ssm_parameter.sqs_card_application_dlq_ssm_param.value
-  description = "The value of the SQS cards queue name stored in SSM"
+  description = "The value of the SQS card application DLQ name stored in SSM"
 }
