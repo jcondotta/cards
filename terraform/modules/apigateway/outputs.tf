16275e836e91e158c6dd3da3ad211d6f2cb550c9
jcondotta <jefferson.condotta@gmail.com>
2025-02-06 22:58:29 +0100

    Removing API gateway terraform files

--- terraform/modules/apigateway/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "api_gateway_id" {
-  description = "The ID of the API Gateway"
-  value       = aws_api_gateway_rest_api.cards_api.id
-}
-
-output "api_gateway_invoke_url" {
-  description = "Invoke URL for the API Gateway"
-  value       = aws_api_gateway_deployment.cards_api_deployment.invoke_url
-}
